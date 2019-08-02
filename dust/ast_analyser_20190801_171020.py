976ea7a94380caa20e424ca1a954fb5f48155c63
Likianta <likianta@foxmail.com>
2019-08-02 13:10:35 +0800

    support parsing args

---
+++ dust/ast_analyser_20190801_171020.py
@@ -0,0 +1,278 @@
+from _ast import *
+from ast import parse as ast_parse, walk as ast_walk
+
+
+class AstAnalyser:
+    file = ''
+    root = None
+    
+    def __init__(self, file):
+        self.file = file
+        
+        with open(file, mode='r', encoding='utf-8-sig') as f:
+            text = f.read()
+        self.root = ast_parse(text)
+    
+    def get_lino_indent_dict(self):
+        """
+        IN: self.file
+            self.root
+        OT: {lino: indent}
+                lino: int. count from 1 but not consecutive. the linos are
+                    already sorted by ascending order.
+                indent: int. the column offset, assert all of them would be
+                    integral multiple of 4, e.g. 0, 4, 8, 12, ... and no
+                    exception.
+        """
+        import re
+        from lk_utils.read_and_write_basic import read_file_by_line
+        
+        reg = re.compile(r'^ *')
+        code_lines = read_file_by_line(self.file)
+        
+        lino_indent = {}
+        
+        for node in ast_walk(self.root):
+            if not hasattr(node, 'lineno'):
+                continue
+            line = code_lines[node.lineno - 1]
+            indent = reg.findall(line)[0]
+            lino_indent[node.lineno] = len(indent)
+        
+        # sort linos
+        sorted_lino_indent = {
+            k: lino_indent[k]
+            for k in sorted(lino_indent.keys())
+        }
+        
+        return sorted_lino_indent
+    
+    def get_lino_indent_dict2(self):
+        """
+        another way calculating indent by ast column_offset.
+        IN: self.file
+            self.root
+        OT: {lino: indent}
+        """
+        lino_indent = {}
+        
+        for node in ast_walk(self.root):
+            if not (hasattr(node, 'lineno') and hasattr(node, 'col_offset')):
+                continue
+            if node.lineno in lino_indent:
+                continue
+            lino_indent[node.lineno] = node.col_offset
+        
+        # sort linos
+        sorted_lino_indent = {
+            k: lino_indent[k]
+            for k in sorted(lino_indent.keys())
+        }
+        
+        return sorted_lino_indent
+    
+    def main(self):
+        """
+        IN: self.root
+        OT: dict. {
+                lino: [(node_type, node_value), (...), ...], ...
+            }
+                lino: int. count from 1 but not consecutive. the linos are
+                    already sorted by ascending order.
+                node_type: str. refer to _ast class types, e.g. "<class '_ast
+                    .Import'>", "<class '_ast.FunctionDef'>", ...
+                node_value: str/dict. e.g. 'os.path.abspath', {'os': 'os'}, ...
+        """
+        out = {}
+        
+        for node in ast_walk(self.root):
+            if not hasattr(node, 'lineno'):
+                continue
+            x = out.setdefault(node.lineno, [])
+            x.append((str(type(node)), self.eval_node(node)))
+        
+        # sort linos
+        sorted_out = {
+            k: out[k]
+            for k in sorted(out.keys())
+        }
+        
+        return sorted_out
+    
+    def eval_node(self, node):
+        result = None
+        
+        while result is None:
+            # lk.loga(type(node))
+            # ------------------------------------------------ output result
+            if isinstance(node, ClassDef):
+                result = node.name
+            elif isinstance(node, FunctionDef):
+                result = node.name
+            elif isinstance(node, Name):
+                result = node.id
+            elif isinstance(node, Str):
+                result = node.s
+            # ------------------------------------------------ compound obj
+            elif isinstance(node, Assign):
+                result = {}
+                a, b = node.targets, node.value
+                k = self.eval_node(b)
+                for i in a:
+                    v = self.eval_node(i)
+                    result[v] = k
+            elif isinstance(node, Attribute):
+                """
+                _fields = ('value', 'attr', 'ctx')
+                    value -> _ast.Name / _ast.Attribute
+                    attr  -> <str>
+                    ctx   -> _ast.Load
+                """
+                # print('[Attribute fields]', node.value, node.attr, node.ctx)
+                v = node.attr
+                k = self.eval_node(node.value)
+                result = k + '.' + v
+                # | result = node.attr
+            elif isinstance(node, Import):
+                result = {}  # {module: import_name_or_asname}
+                for imp in node.names:
+                    if imp.asname is None:
+                        result[imp.name] = imp.name
+                    else:
+                        result[imp.name] = imp.asname
+            elif isinstance(node, ImportFrom):
+                result = {}  # {module: import_name_or_asname}
+                module = node.module
+                for imp in node.names:
+                    if imp.asname is None:
+                        result[module + '.' + imp.name] = imp.name
+                    else:
+                        result[module + '.' + imp.name] = imp.asname
+            # ------------------------------------------------ take reloop
+            elif isinstance(node, Call):
+                """
+                _fields = ('func', 'args', 'keywords')
+                    func     -> _ast.Attribute / _ast.Name
+                    args     -> [] (empty list) / [_ast.Call]
+                    keywords -> [] (empty list)
+                """
+                # print('[Call fields]', node.func, node.args, node.keywords)
+                node = node.func
+            elif isinstance(node, Expr):
+                node = node.value
+            elif isinstance(node, Subscript):
+                node = node.value
+            else:
+                # noinspection PyProtectedMember
+                result = str(node._fields)
+        
+        return result
+
+
+# ------------------------------------------------
+
+def dump_asthelper_result():
+    """
+    将 AstHelper 解析结果输出到 json 文件.
+    
+    IN: temp/in.py
+            suggest copied from testflight/test_app_launcher.py
+    OT: temp/out.json
+            backup this file to res/sample/test_app_launcher(ast_helper_result)
+            .json
+    """
+    from lk_utils.read_and_write_basic import write_json
+    helper = AstAnalyser('../temp/in.py')
+    res = helper.main()
+    write_json(res, '../temp/out.json')
+
+
+def dump_lino_indent_result():
+    """
+    将 AstHelper#get_lino_indent_dict() 的结果输出到 json 文件.
+    IN: temp/in.py
+    OT: temp/out.json
+    """
+    from lk_utils.read_and_write_basic import write_json
+    helper = AstAnalyser('../temp/in.py')
+    
+    # TEST choice
+    # res = helper.get_lino_indent_dict()
+    res = helper.get_lino_indent_dict2()
+    
+    write_json(res, '../temp/out.json')
+
+
+def dump_by_filter_schema(file, schema=1):
+    """
+    将 AstHelper 解析结果根据对象类型 (库, 变量, 方法和类对象) 分类后, 输出或打印出来.
+
+    IN: file: str. 要解析的 py 文件, 传入绝对路径或相对路径. e.g. './dump_asthelper_result.py'
+    OT: schema 1:
+            print out to the console
+        schema 2:
+            dump collector to './ast_helper_result.json'
+    """
+    helper = AstAnalyser(file)
+    res = helper.main()
+    
+    lib_dict = {}
+    var_dict = {}
+    fun_dict = {}
+    cls_dict = {}
+    
+    dict_filter = {
+        "<class '_ast.Import'>"     : lib_dict,
+        "<class '_ast.ImportFrom'>" : lib_dict,
+        "<class '_ast.Assign'>"     : var_dict,
+        "<class '_ast.FunctionDef'>": fun_dict,
+        "<class '_ast.ClassDef'>"   : cls_dict,
+    }
+    
+    for lino, data in res.items():
+        for i in data:
+            type_, value = i
+            if type_ in dict_filter:
+                d = dict_filter.get(type_)
+                if isinstance(value, str):
+                    # schema 1: use list to store vars
+                    node = d.setdefault(value, [])
+                    node.append(lino)
+                    # schema 2: override if the old key-value exists
+                    # d[value] = lino
+                else:
+                    for k in value.keys():
+                        # schema 1: use list to store vars
+                        node = d.setdefault(k, [])
+                        node.append(lino)
+                        # schema 2: override if the old key-value exists
+                        # d[k] = lino
+    
+    if schema == 1:
+        # schema 1: print out to the console
+        print('库', lib_dict)
+        print('类', cls_dict)
+        print('函数', fun_dict)
+        print('变量', var_dict)
+    else:
+        # schema 2: dump to local file
+        from json import dumps
+        
+        out = {
+            "lib_dict": lib_dict,
+            "var_dict": var_dict,
+            "fun_dict": fun_dict,
+            "cls_dict": cls_dict,
+        }
+        
+        with open('ast_helper_result.json', encoding='utf-8', mode='w') as f:
+            f.write(dumps(out, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    # dump_asthelper_result()
+    
+    dump_lino_indent_result()
+    
+    # 在这里传入要解析的 py 文件的路径.
+    # dump_by_filter_schema('dump_asthelper_result.py')
--- src/analyser.py
+++ src/analyser.py
@@ -15,6 +15,7 @@ from os.path import abspath, exists
 from lk_utils import file_sniffer
 from lk_utils.lk_logger import lk
 
+from src.assign_analyser import AssignAnalyser
 from src.ast_analyser import AstAnalyser
 from src.module_analyser import ModuleAnalyser
 from src.writer import Writer
@@ -70,7 +71,9 @@ class VirtualRunner:
         self.module_linos = module_analyser.indexing_module_linos()
         # -> {module: [lino, ...]}
         
-        self.assign_analyser = AssignAnalyser()
+        self.assign_analyser = AssignAnalyser(
+            module_analyser, ast_tree, ast_indents
+        )
         
         self.assign_reachables = self.assign_analyser.top_assigns_prj_only
         # -> {var: module}
@@ -80,6 +83,7 @@ class VirtualRunner:
         self.outer_call_chain = []
         
         self.registered_methods = {
+            "<class '_ast.arg'>"      : self.parse_arg,
             "<class '_ast.Assign'>"   : self.parse_assign,
             "<class '_ast.Attribute'>": self.parse_attribute,
             "<class '_ast.Call'>"     : self.parse_call,
@@ -215,6 +219,9 @@ class VirtualRunner:
         # lk.logt('[TEMPRINT]', 'nothing todo', data)
         pass
     
+    def parse_arg(self, arg: str):
+        pass
+        
     def parse_assign(self, assign: dict):
         """
         IN: assign: e.g. {"init": "Init"}
@@ -303,145 +310,6 @@ class VirtualRunner:
         lk.logt('[E1036]', 'a function def found in block region, this should '
                            'not be happend', data)
         raise Exception
-
-
-class AssignAnalyser:
-    
-    def __init__(self):
-        self.prj_modules = module_analyser.prj_modules
-        
-        self.max_lino = max(ast_indents.keys())
-        lk.loga(self.max_lino)
-        
-        self.top_linos = [
-            lino
-            for lino, indent in ast_indents.items()
-            if indent == 0
-        ]
-        
-        runtime_module = module_analyser.get_runtime_module()
-        self.top_assigns = self.update_assigns(runtime_module, self.top_linos)
-        # 注意: self.top_assigns 是包含非项目模块的.
-        # -> {'os': 'os', 'downloader': 'testflight.downloader', 'Parser':
-        # 'testflight.parser.Parser', 'main': 'testflight.app.main', 'Init':
-        # 'testflight.app.Init'}
-        self.top_assigns_prj_only = self.get_only_prj_modules(self.top_assigns)
-        lk.loga(self.top_assigns)
-        lk.loga(self.top_assigns_prj_only)
-    
-    @staticmethod
-    def update_assigns(target_module, linos):
-        assigns = {}
-        
-        module_linos = module_analyser.indexing_module_linos(
-            module_analyser.get_parent_module(target_module), linos
-        )
-        # 注意: 这里第一个参数传入 get_parent_module(module) 而非 module. 原因详见 src
-        # .analyser.ModuleAnalyser#indexing_module_linos() 注释.
-        
-        for module in module_linos.keys():
-            if module == target_module:
-                """
-                因为 target_module 不能指任自身, 所以应去除.
-                例如 target_module = 'src.app.module', 在源码中, 不能因此自动产生
-                module-src.app.module 的对应关系. 所以不能加入到 assigns 中.
-                """
-                continue
-            var = module.rsplit('.', 1)[1]
-            # lk.logt('[TEMPRINT]', var, module)
-            assigns[var] = module
-        
-        # ------------------------------------------------
-        
-        # ABBR: defs: definitions. imps: imports.
-        # ast_defs = ("<class '_ast.FunctionDef'>", "<class '_ast.ClassDef'>")
-        ast_imps = ("<class '_ast.Import'>", "<class '_ast.ImportFrom'>")
-        
-        for lino in linos:
-            ast_line = ast_tree.get(lino)
-            # lk.logt('[TEMPRINT]', lino, ast_line)
-            # -> [(obj_type, obj_val), ...]
-            
-            for element in ast_line:
-                obj_type, obj_val = element
-                # lk.logt('[TEMPRINT]', obj_type, obj_val)
-                if obj_type in ast_imps:
-                    for k, v in obj_val.items():
-                        module = k
-                        var = v
-                        assigns[var] = module
-        return assigns
-    
-    def indexing_assign_reachables(
-            self, target_module, module_linos, only_prj_modules=True
-    ):
-        if target_module == module_analyser.get_runtime_module():
-            return self.top_assigns_prj_only if only_prj_modules \
-                else self.top_assigns
-        
-        # ------------------------------------------------
-        
-        target_linos = module_linos[target_module]
-        start_offset, end_offset = target_linos[0], target_linos[-1] + 1
-        indent = ast_indents[start_offset]
-        if indent == 0:
-            module = target_module
-        else:
-            while True:
-                parent_module = module_analyser.get_parent_module(target_module)
-                parent_linos = module_linos[parent_module]
-                start_offset, end_offset = parent_linos[0], parent_linos[-1] + 1
-                parent_indent = ast_indents[start_offset]
-                if parent_indent == 0:
-                    break
-                else:
-                    continue
-            module = parent_module
-
-        while end_offset < self.max_lino:
-            if end_offset in ast_indents:
-                indent = ast_indents[end_offset]
-                if indent == 0:
-                    break
-            end_offset += 1
-
-        lino_reachables = [
-            lino
-            for lino in range(start_offset, end_offset)
-            if lino in ast_indents
-        ]
-        
-        # ------------------------------------------------
-        
-        if only_prj_modules:
-            assigns = self.top_assigns_prj_only.copy()
-        else:
-            assigns = self.top_assigns.copy()
-        
-        assigns.update(
-            self.update_assigns(
-                module, lino_reachables
-            )
-        )
-        
-        # FIXME: dirty code
-        var = target_module.rsplit('.', 1)[1]
-        if assigns.get(var) == target_module:
-            assigns.pop(var)
-        
-        if only_prj_modules:
-            return self.get_only_prj_modules(assigns)
-        else:
-            return assigns
-    
-    def get_only_prj_modules(self, assigns: dict):
-        new_assigns = {}
-        for var, module in assigns.items():
-            for prj_module in self.prj_modules:
-                if module.startswith(prj_module):
-                    new_assigns[var] = module
-                    break
-        return new_assigns
 
 
 # ------------------------------------------------
